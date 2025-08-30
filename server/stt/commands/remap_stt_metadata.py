1e99159c9e24b1af664ad0e82ae5618496d317c1
Helmy Giacoman <helmygb@gmail.com>
2025-08-30 21:47:52 +0200

    Add STT media topics data and update remap script
    Introduces a new `STT_Media_Topics.json` file containing a list of media topics according to ticket STT-1210.
    Updates `remap_stt_metadata.py` to map the `sttsubj` with new media topics.
    
    STT-1223

--- server/stt/commands/remap_stt_metadata.py
+++ server/stt/commands/remap_stt_metadata.py
@@ -1,5 +1,7 @@
 import time
+import json
 
+from pathlib import Path
 from dataclasses import dataclass
 from superdesk import get_resource_service
 from newsroom.commands.manager import manager
@@ -9,6 +11,67 @@ from newsroom.commands.manager import manager
 class DefaultCategory:
     code: str = "3"  # NOTE: is 3 always for `Kotimaa` category?
     name: str = "Kotimaa"
+
+
+# Load topics CV from json file based on STT-1210
+topics_cv_path = Path(__file__).parent / "data" / "STT_Media_Topics.json"
+with open(topics_cv_path, "r", encoding="utf-8") as f:
+    topics_cv = json.load(f)
+topics_map = {
+    item.get("iptc_subject"): item
+    for item in topics_cv.get("items", [])
+    if item.get("iptc_subject")
+}
+
+# used as fallback in case the code is not found
+topics_by_name = {
+    item.get("name"): item
+    for item in topics_cv.get("items", [])
+    if item.get("iptc_subject")
+}
+
+
+def update_category(item, updates):
+    """Update anpa_category and sttversion from subject (sttdepartment)."""
+
+    subject = item.get("subject", [])
+    for entry in subject:
+        if entry.get("scheme") == "sttdepartment":
+            updates["anpa_category"] = [
+                {"qcode": entry.get("code"), "name": entry.get("name")}
+            ]
+            return
+    updates["anpa_category"] = [
+        {"qcode": DefaultCategory.code, "name": DefaultCategory.name}
+    ]
+    updates["sttversion"] = "Pika+"
+
+
+def update_subject(item, updates):
+    """Update subject by mapping sttsubj to Media topics CV."""
+
+    subject = item.get("subject", [])
+    new_subjects = [entry for entry in subject if entry.get("scheme") != "sttsubj"]
+
+    for entry in subject:
+        if entry.get("scheme") == "sttsubj":
+            code = entry.get("code")
+            name = entry.get("name")
+            topic = topics_map.get(code) or topics_by_name.get(name)
+
+            if topic:
+                new_subjects.append(
+                    {
+                        "code": topic.get("qcode"),
+                        "name": topic.get("name"),
+                        "scheme": "sttsubj", # NOTE: should it be something else?
+                    }
+                )
+            else:
+                print(f"Topic not found for '{name}' and code '{code}'.")
+                new_subjects.append({"code": code, "name": name, "scheme": "sttsubj"})
+
+    updates["subject"] = new_subjects
 
 
 def get_category_mapping(item):
@@ -26,7 +89,8 @@ def get_category_mapping(item):
 @manager.option("--limit", dest="limit", type=int, default=500)
 @manager.option("--sleep-secs", dest="sleep_secs", type=float, default=2)
 @manager.option("--dry-run", dest="dry_run", action="store_true")
-def remap_stt_metadata(resources, limit, sleep_secs, dry_run):
+@manager.option("-v", "--verbose", dest="verbose", action="store_true")
+def remap_stt_metadata(resources, limit, sleep_secs, dry_run, verbose):
     """Remap sttdepartment to anpa_category for Wire and Agenda items in Newsroom."""
 
     for resource in resources:
@@ -40,16 +104,20 @@ def remap_stt_metadata(resources, limit, sleep_secs, dry_run):
                 break
 
             updates = {}
-            category = get_category_mapping(item)
-            updates["anpa_category"] = [category]
+            update_category(item, updates)
+            update_subject(item, updates)
 
-            if category.get("name") == DefaultCategory.name:
-                updates["sttversion"] = "Pika+"
+            if not updates:
+                continue
 
-            if dry_run:
-                print(f"Would update {resource} item {item['_id']} with {updates}")
-            else:
-                print(f"Updating {resource} item {item['_id']} with {updates}")
+
+            prefix = "DRY RUN: " if dry_run else ""
+            msg = f"{prefix}Updating {resource} item '{item['_id']}'"
+            if verbose:
+                msg += f" with {updates}"
+            print(msg)
+
+            if not dry_run:
                 service.system_update(item["_id"], updates, item)
 
             processed += 1
