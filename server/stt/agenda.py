067ddf90529effa991a1fb4942dc1f056a8d222c
Mark Pittaway <mark.pittaway@gmail.com>
2025-10-30 14:06:08 +1100

    fix lint issue

--- server/stt/agenda.py
+++ server/stt/agenda.py
@@ -4,9 +4,12 @@ from newsroom.agenda.filters import PRIVATE_FIELDS
 
 def init_module(app: SuperdeskAsyncApp):
     if "coverages.scheduled" not in PRIVATE_FIELDS:
-        PRIVATE_FIELDS.extend([
-            "coverages.scheduled",
-            "planning_items.coverages.planning.scheduled",
-        ])
+        PRIVATE_FIELDS.extend(
+            [
+                "coverages.scheduled",
+                "planning_items.coverages.planning.scheduled",
+            ]
+        )
+
 
 module = Module(name="stt.agenda", init=init_module)
